1b2a83297ba863f7dd73aaf0a32e0b4dc72c430f
WasimKaunain <ansariw580@gmail.com>
2026-04-27 18:37:24 +0530

    removed disabled apis

--- api/_disabled_hobby/whatsapp-send.ts
+++
@@ -1,8 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default async function handler(_req: VercelRequest, res: VercelResponse) {
-  return res.status(410).json({
-    error: "Removed",
-    message: "This endpoint has been removed. Voucher WhatsApp delivery happens automatically on voucher creation.",
-  });
-}
